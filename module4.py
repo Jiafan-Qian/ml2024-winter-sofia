31f51f009386ff6108539f37d370494e55c961c1
jiafan <jiqi7875@gmail.com>
2024-02-02 00:00:10 -0800

    simple program to find index of the value in an array

---
+++ module4.py
@@ -0,0 +1,28 @@
+#MSCS 2202 module 4 Machine Learning Assignment
+# Author: Jiafan Qian
+# Created at 02/01/2024
+
+# note that the following code requires user to input integers
+# decimal numbers or negative numbers may cause errors
+
+# asks the user for input N (positive integer) and reads it
+N = int(input("Please enter a positive integer: "))
+print(f"Integer you input: {N}")
+
+# asks the user to provide N numbers (one by one) and reads all of them (again, one by one)
+print(f"Please provide {N} integer numbers one by one")
+numbers = []
+for i in range(N):
+    number = int(input(f"Please enter number {i+1}: "))
+    print("Your input: ", number)
+    numbers.append(number)
+
+# asks the user for input X (integer)
+X = int(input("Please enter an integer: "))
+
+# outputs: "-1" if there were no such X among N read numbers, 
+# or the index (from 1 to N) of this X if the user inputed it before
+if X in numbers:
+    print(numbers.index(X))
+else:
+    print("-1")
